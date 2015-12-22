edd524a46fc26c0a13c855a84d565f35b18d4ad9
justins <justins@wealthfront.com>
2015-12-21 22:05:41 -0800

    Support parsing of floating point types

--- src/pg-row-data-parser.js
+++ src/pg-row-data-parser.js
@@ -2,11 +2,21 @@ const parseIntType = (buf) => {
   return parseInt(buf.toString('utf8'))
 }
 
-const dataTypeIdToParseFnc = {
-  20: parseIntType,
-  21: parseIntType,
-  23: parseIntType
+const parseFloatType = (buf) => {
+  return parseFloat(buf.toString('utf8'))
 }
+
+}
+
+const dataTypeIdToParseFnc = new Map()
+
+dataTypeIdToParseFnc.set(20, parseIntType)
+dataTypeIdToParseFnc.set(21, parseIntType)
+dataTypeIdToParseFnc.set(23, parseIntType)
+dataTypeIdToParseFnc.set(700, parseFloatType)
+dataTypeIdToParseFnc.set(701, parseFloatType)
+dataTypeIdToParseFnc.set(1700, parseFloatType)
+dataTypeIdToParseFnc.set(16, parseBoolType)
 
 /**
  * Takes dataTypeId and buffer and returns a parsed column value [Integer | Float | boolean]
@@ -14,5 +24,5 @@ const dataTypeIdToParseFnc = {
  * buf: Column data in binary
 **/
 export const parseColumnValue = (dataTypeId, buf) => {
-  return dataTypeIdToParseFnc[dataTypeId](buf)
+  return dataTypeIdToParseFnc.get(dataTypeId)(buf)
 }
--- test/src/unit/pg-row-data-parser-test.js
+++ test/src/unit/pg-row-data-parser-test.js
@@ -42,5 +42,54 @@ describe('Row data parser', () => {
         })
       })
     })
+
+    describe('floating point types', () => {
+      describe('real type', () => {
+        const asBuffer = new Buffer('123.456', 'utf8')
+
+        it('parses a positive float4', () => {
+          const rowData = asBuffer
+          assert.deepEqual(parseColumnValue(700, rowData), 123.456)
+        })
+
+        it('parses a negative float4', () => {
+          const rowData = Buffer.concat([new Buffer([0x2d]), asBuffer])
+          assert.deepEqual(parseColumnValue(700, rowData), -123.456)
+        })
+      })
+
+      describe('double precision type', () => {
+        const asBuffer = new Buffer('12345678.12345678', 'utf8')
+
+        it('parses a positive float8', () => {
+          const rowData = asBuffer
+          assert.deepEqual(parseColumnValue(701, rowData), 12345678.12345678)
+        })
+
+        it('parses a negative float8', () => {
+          const rowData = Buffer.concat([new Buffer([0x2d]), asBuffer])
+          assert.deepEqual(parseColumnValue(701, rowData), -12345678.12345678)
+        })
+      })
+    })
+
+    describe('Arbitrary precision types', () => {
+      describe('numeric (aka decimal) type', () => {
+        const bignumAsStr = '31415926535897932384626433832795028841971693993751058.16180339887498948482045868343656381177203091798057628'
+        const bignumAsNum = 31415926535897932384626433832795028841971693993751058.16180339887498948482045868343656381177203091798057628
+        const asBuffer = new Buffer(bignumAsStr, 'utf8')
+
+        it('parses a positive bignum', () => {
+          const rowData = asBuffer
+          assert.deepEqual(parseColumnValue(1700, rowData), bignumAsNum)
+        })
+
+        it('parses a negative bignum', () => {
+          const rowData = Buffer.concat([new Buffer([0x2d]), asBuffer])
+          assert.deepEqual(parseColumnValue(1700, rowData), -1 * bignumAsNum)
+        })
+      })
+    })
+
   })
 })
