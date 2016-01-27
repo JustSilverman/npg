8f250d6e2238ad44ba0a60bb115814b79af2a7f2
justins <justins@wealthfront.com>
2016-01-27 12:09:25 -0800

    Add msg-sequence to create a message iterator from buffer

---
+++ src/msg-sequence.js
@@ -0,0 +1,60 @@
+import * as assert from 'assert'
+import * as meta from './meta'
+import { read }  from './msg-reader'
+import { hexBuf }  from './hex-buf'
+
+meta.module(module, {
+  doc: `
+    # Binary message sequence
+
+    Creates a message iterator from a buffer
+  `,
+})
+
+// -
+
+meta.fn('read', {
+  doc: 'Read a sequence of messages from a buffer',
+  shape: 'buffer, int?, int?, bool? -> [ iterator?, function ]',
+  args: [
+    'buffer with messages data in it',
+    'size of message header, defaults to 1, optional',
+    'size of message length bytes, defaults to 4, optional',
+    'whether the messages length include the length bytes, defaults to true, optional',
+  ],
+  returns: [
+    'message iterator',
+    'function that returns remaining bytes',
+  ],
+  examples: {
+    '2 messages, 3 byte remainder': (f) => {
+      const messagesBuf = hexBuf('0a 00 00 00 05 0b 0c 00 00 00 07 03 0b 10 0a 00 01')
+      const [ messages, rest ] = f(messagesBuf)
+      assert.deepEqual(messages.next().value, { head: hexBuf('0a'), body: hexBuf('0b') })
+      assert.deepEqual(messages.next().value, { head: hexBuf('0c'), body: hexBuf('03 0b 10') })
+      assert.deepEqual(rest(), hexBuf('0a 00 01'))
+      assert.equal(messages.next().done, true)
+    }
+  },
+})
+
+export const readSeq = (buf, headLength = 1, lengthBytesCount = 4, lengthBytesInclusive = true) => {
+  let message
+  let remainder = buf
+
+  const seqGenerator = function* () {
+    while (remainder.length) {
+      [ message, remainder ] = read(remainder, headLength, lengthBytesCount, lengthBytesInclusive)
+      yield message
+    }
+  }
+
+  return [
+    seqGenerator(),
+    () => {
+      const rest = remainder
+      remainder = new Buffer([])
+      return rest
+    }
+  ]
+}
---
+++ test/src/unit/msg-sequence-test.js
@@ -0,0 +1,91 @@
+import { equal, deepEqual } from 'assert'
+import { readSeq } from '../../../src/msg-sequence'
+import { hexBuf } from '../../../src/hex-buf'
+
+describe('Message sequence', () => {
+  describe('#readSeq', () => {
+    it('reads one message and calls rest', () => {
+      const given = hexBuf('0a 00 00 00 05 0b 0c 00 00 00 07 03 0b 10 0a')
+      const expectedMessage = {
+        head: hexBuf('0a'),
+        body: hexBuf('0b')
+      }
+      const expectedRest = hexBuf('0c 00 00 00 07 03 0b 10 0a')
+
+      const [ messages, rest ] = readSeq(given)
+      deepEqual(messages.next().value, expectedMessage)
+      deepEqual(rest(), expectedRest)
+      equal(messages.next().done, true)
+    })
+
+    it('reads all messages including partial message and calls rest with remainder', () => {
+      const given = hexBuf('0a 00 00 00 05 0b 0c 00 00 00 07 03 0b 10 00 0a')
+      const expectedMessages = [
+        {
+          head: hexBuf('0a'),
+          body: hexBuf('0b')
+        },
+        {
+          head: hexBuf('0c'),
+          body: hexBuf('03 0b 10')
+        }
+      ]
+
+      const [ messages, rest ] = readSeq(given)
+      deepEqual(messages.next().value, expectedMessages[0])
+      deepEqual(messages.next().value, expectedMessages[1])
+      deepEqual(rest(), hexBuf('00 0a'))
+      equal(messages.next().done, true)
+    })
+
+    it('reads all messages and calls rest', () => {
+      const given = hexBuf('0a 00 00 00 05 0b 0c 00 00 00 07 03 0b 10')
+      const expectedMessages = [
+        {
+          head: hexBuf('0a'),
+          body: hexBuf('0b')
+        },
+        {
+          head: hexBuf('0c'),
+          body: hexBuf('03 0b 10')
+        }
+      ]
+
+      const [ messages, rest ] = readSeq(given)
+      deepEqual(messages.next().value, expectedMessages[0])
+      deepEqual(messages.next().value, expectedMessages[1])
+      deepEqual(rest(), hexBuf(''))
+      equal(messages.next().done, true)
+    })
+
+    it('reads all messages (in for of loop) and calls rest', () => {
+      const given = hexBuf('0a 00 00 00 05 0b 0c 00 00 00 07 03 0b 10')
+      const expectedMessages = [
+        {
+          head: hexBuf('0a'),
+          body: hexBuf('0b')
+        },
+        {
+          head: hexBuf('0c'),
+          body: hexBuf('03 0b 10')
+        }
+      ]
+
+      const result = []
+      const [ messages, rest ] = readSeq(given)
+      for (let msg of messages) {
+        result.push(msg)
+      }
+      deepEqual(result, expectedMessages)
+      deepEqual(messages.next().done, true)
+      deepEqual(rest(), hexBuf(''))
+    })
+
+    it('reads no messages and calls rest', () => {
+      const given = hexBuf('')
+      const [ messages, rest ] = readSeq(given)
+      deepEqual(rest(), hexBuf(''))
+      equal(messages.next().done, true)
+    })
+  })
+})
