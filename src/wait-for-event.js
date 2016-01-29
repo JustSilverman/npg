68a6bc9d57f69c03c688c26e3f771dfdc0ee5f34
justins <justins@wealthfront.com>
2016-01-29 11:24:13 -0800

    Add wait-for-event to help with async control flow

---
+++ src/wait-for-event.js
@@ -0,0 +1,77 @@
+import * as assert from 'assert'
+import * as  EventEmitter from 'events';
+import * as meta from './meta'
+import { hexBuf }  from './hex-buf'
+
+meta.module(module, {
+  doc: `
+    # Control flow helper
+
+    Transforms waiting for an event into a promise
+  `,
+})
+
+// -
+
+meta.fn('waitForEvent', {
+  doc: 'Return a promise that resolves when emitter emits event',
+  shape: 'emitter, str, int? -> promise',
+  args: [
+    'event emitter',
+    'event type that causes promise to be resolved',
+    'timeout duration in ms'
+  ],
+  returns: [
+    'Promise',
+      'resolves when emitter emits eventType',
+      'rejected if emitter emits error',
+      'rejected if timeout is reached'
+  ],
+  examples: {
+    'resolve the promise on a data event': (f) => {
+      const givenEventType = 'data'
+      const expected = hexBuf('00 01 02')
+      const emitter = new EventEmitter()
+
+      f(emitter, givenEventType)
+        .then(buf => { assert.deepEqual(buf, given) })
+        .catch(err => { throw err })
+
+      emitter.emit('data', expected)
+    }
+  },
+})
+
+export const waitForEvent = (emitter, event, timeoutDuration = 1000) => {
+  const TIMEOUT_ERROR = new Error('Timeout of ' + timeoutDuration + ' ms reached waiting for ' + event + ' to be emitted.')
+
+  return new Promise((resolve, reject) => {
+    const timeoutId = setTimeout(() => {
+      reject(TIMEOUT_ERROR)
+      removeListeners()
+    }, timeoutDuration)
+
+
+    const handleEvent = (arg) => {
+      resolve(arg)
+      removeListeners()
+    }
+
+    const handleError = (err) => {
+      reject(err)
+      removeListeners()
+    }
+
+    const removeListeners = () => {
+      emitter.removeListener(event, handleEvent)
+      emitter.removeListener('error', handleError)
+    }
+
+
+    emitter
+      .on(event, handleEvent)
+      .on('error', handleError)
+  })
+}
+
+export default waitForEvent
---
+++ test/src/unit/wait-for-event-test.js
@@ -0,0 +1,136 @@
+import { equal, deepEqual, fail } from 'assert'
+import { EventEmitter } from 'events';
+import { hexBuf } from '../../../src/hex-buf'
+import { waitForEvent } from '../../../src/wait-for-event'
+
+describe('wait-for-event', () => {
+  describe('#waitForEvent', () => {
+    it('resolves the promise with one argument when the event is emitted', (done) => {
+      const givenEmitter = new EventEmitter()
+      const givenEventType = 'data'
+      const givenEventData = hexBuf('00 01 02')
+      const expectedData = givenEventData
+
+      waitForEvent(givenEmitter, givenEventType)
+        .then(buf => {
+          deepEqual(buf, expectedData)
+          done()
+        })
+        .catch(fail)
+      deepEqual(givenEmitter.listenerCount(givenEventType), 1)
+      deepEqual(givenEmitter.listenerCount('error'), 1)
+
+      givenEmitter.emit(givenEventType, givenEventData)
+    })
+
+    it('rejects the promise when error is emitted', (done) => {
+      const givenEmitter = new EventEmitter()
+      const givenEventType = 'data'
+      const givenError = new Error('Internal error')
+      const expectedError = givenError
+
+      waitForEvent(givenEmitter, givenEventType)
+        .then(fail)
+        .catch(error => {
+          deepEqual(error, expectedError)
+          done()
+        })
+      deepEqual(givenEmitter.listenerCount(givenEventType), 1)
+      deepEqual(givenEmitter.listenerCount('error'), 1)
+
+      givenEmitter.emit('error', givenError)
+    })
+
+    it('rejects the promise with a timeout error if the timeout is reached', (done) => {
+      const givenEmitter = new EventEmitter()
+      const givenEventType = 'data'
+      const givenDurationUntilEvent = 1001
+      const expectedError = new Error('Timeout of 1000ms reached waiting for ' + givenEventType + ' to be emitted.')
+
+      waitForEvent(givenEmitter, givenEventType)
+        .then(fail)
+        .catch(error => {
+          deepEqual(error, expectedError)
+          done()
+        })
+
+      setTimeout(givenEmitter.emit.bind(givenEmitter, givenEventType), givenDurationUntilEvent)
+    })
+
+    it('rejects the promise with a custom timeout error if the timeout is reached', (done) => {
+      const givenEmitter = new EventEmitter()
+      const givenEventType = 'data'
+      const givenDurationUntilEvent = 500
+      const expectedError = new Error('Timeout of 400ms reached waiting for ' + givenEventType + ' to be emitted.')
+
+      waitForEvent(givenEmitter, givenEventType, 400)
+        .then(fail)
+        .catch(error => {
+          deepEqual(error, expectedError)
+          done()
+        })
+
+      setTimeout(givenEmitter.emit.bind(givenEmitter, givenEventType), givenDurationUntilEvent)
+    })
+
+    it('removes listeners upon resolution', (done) => {
+      const givenEmitter = new EventEmitter()
+      const givenEventType = 'data'
+      const givenEventData = hexBuf('00 01 02')
+      const expectedData = givenEventData
+
+      waitForEvent(givenEmitter, givenEventType)
+        .then(buf => {
+          deepEqual(buf, expectedData)
+          deepEqual(givenEmitter.listenerCount(givenEventType), 0)
+          deepEqual(givenEmitter.listenerCount('error'), 0)
+          done()
+        })
+        .catch(fail)
+      deepEqual(givenEmitter.listenerCount(givenEventType), 1)
+      deepEqual(givenEmitter.listenerCount('error'), 1)
+
+      givenEmitter.emit(givenEventType, givenEventData)
+    })
+
+    it('removes listeners upon rejection from error', (done) => {
+      const givenEmitter = new EventEmitter()
+      const givenEventType = 'data'
+      const givenError = new Error('Internal error')
+      const expectedError = givenError
+
+      waitForEvent(givenEmitter, givenEventType)
+        .then(fail)
+        .catch(error => {
+          deepEqual(error, expectedError)
+          deepEqual(givenEmitter.listenerCount(givenEventType), 0)
+          deepEqual(givenEmitter.listenerCount('error'), 0)
+          done()
+        })
+      deepEqual(givenEmitter.listenerCount(givenEventType), 1)
+      deepEqual(givenEmitter.listenerCount('error'), 1)
+
+      givenEmitter.emit('error', givenError)
+    })
+
+    it('removes listeners upon rejection from timeout', (done) => {
+      const givenEmitter = new EventEmitter()
+      const givenEventType = 'data'
+      const givenDurationUntilEvent = 1001
+      const expectedError = new Error('Timeout of 1000ms reached waiting for ' + givenEventType + ' to be emitted.')
+
+      waitForEvent(givenEmitter, givenEventType)
+        .then(fail)
+        .catch(error => {
+          deepEqual(error, expectedError)
+          deepEqual(givenEmitter.listenerCount(givenEventType), 0)
+          deepEqual(givenEmitter.listenerCount('error'), 0)
+          done()
+        })
+      deepEqual(givenEmitter.listenerCount(givenEventType), 1)
+      deepEqual(givenEmitter.listenerCount('error'), 1)
+
+      setTimeout(givenEmitter.emit.bind(givenEmitter, givenEventType), givenDurationUntilEvent)
+    })
+  })
+})
