53ee4aef0fb55c8577db25bba2370e1f881aec15
justins <justins@wealthfront.com>
2016-02-06 16:12:31 -0800

    Add to msg-headers

--- src/msg-headers.js
+++ src/msg-headers.js
@@ -6,8 +6,18 @@ export const headerBytesToSym = new Map()
 // -
 
 // A entry for each message header we care about
-
+// pass ascii char as arg to symbol
 export const authenticationOk = Symbol()
 map.set(new Buffer([0x52]), authenticationOk)
 
-// etc ...
+export const readyForQuery = Symbol()
+map.set(new Buffer([0x5a]), readyForQuery)
+
+export const sessionConfirm = Symbol()
+map.set(new Buffer([0x4e]), sessionConfirm)
+
+export const backendKeyData = Symbol()
+map.set(new Buffer([0x4b]), backendKeyData)
+
+export const endRows = Symbol()
+map.set(new Buffer([0x43]), endRows)
