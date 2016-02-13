31b903aa0debac94e5baa0f32bf2d4132a9321d2
justins <justins@wealthfront.com>
2016-02-12 22:09:49 -0800

    Update csp-connect

--- src/csp-connect.js
+++ src/csp-connect.js
@@ -1,75 +1,75 @@
-/*
-
-from: http://jlongster.com/Taming-the-Asynchronous-Beast-with-CSP-in-JavaScript
-
-function httpRequest(url) {
-  var ch = chan();
-  var req = new XMLHttpRequest();
-  req.onload = function() {
-    if(req.status === 200) {
-      csp.putAsync(ch, this.responseText);
-    }
-    else {
-      csp.putAsync(ch, new Error(this.responseText));
-    }
-  }
-  req.open('get', url, true);
-  req.send();
-  return ch;
-}
-
-function jsonRequest(url) {
-  return go(function*() {
-    var value = yield take(httpRequest(url));
-    if(!(value instanceof Error)) {
-      value = JSON.parse(value);
-    }
-    return value;
-  });
-}
-
-go(function*() {
-  var data = yield takem(jsonRequest('sample.json'));
-  console.log(JSON.stringify(data));
-});
-
-*/
-
+import { equal } from 'assert'
 import csp from 'js-csp'
 
 const serverToClientMsgs = csp.chan()
 const clientToServerMsgs = csp.chan()
 
 const mockServer = (rChan, wChan) => {
-  return go(function * () {
+  return csp.go(function * () {
     let msg
-    msg = yield csp.take(rChan, '')
-    // assert startup message ...
+    msg = yield csp.take(rChan)
+    equal(msg, 'first startup')
+
     yield csp.put(wChan, 'session confirm')
-    msg = yield csp.take(rChan, '')
-    // assert second startup message ...
+    msg = yield csp.take(rChan)
+    equal(msg, 'second startup')
+
     yield csp.put(wChan, 'info about the server ...')
     yield csp.put(wChan, 'ready for query')
+    yield csp.put(wChan, 'ready for query2')
+    yield csp.put(wChan, 'ready for query3')
 
-    while((msg = yield csp.take(rChan, '') !== 'close') {
+
+    while((msg = yield csp.take(rChan)) !== 'close') {
       let queryMsg = msg
+      console.log('done')
       // do query ...
-      // write results
+      // write result head
+      // write result rows
+      // write result close
     }
-
-    // write close?
   })
 }
 
 const client = (rChan, wChan) => {
-  return go(function * () {
-    // ....
+  return csp.go(function * () {
+    console.log('client started')
+    let msg
+    yield csp.put(wChan, 'first startup')
+    console.log('wrote first startup')
+
+    msg = yield csp.take(rChan)
+    if (msg !== 'session confirm') {
+      throw new Error('Expected session confirm message, but received ', msg)
+    }
+
+    yield csp.put(wChan, 'second startup')
+    console.log('wrote second startup')
+
+    const paramaterStatusMessages = []
+    msg = yield csp.take(rChan)
+    while(msg !== 'backend key data') {
+      console.log('paramaterStatusMessages', msg)
+      paramaterStatusMessages.push(msg)
+      msg = yield csp.take(rChan)
+    }
+
+    const backendKeyDataMsg = msg
+    msg = yield csp.take(rChan)
+    if (msg !== 'ready for query') {
+      throw new Error('Expected ready for query message, but received ', msg)
+    }
+
+    yield csp.put(wChan, 'first query')
+    const queryRows = []
+    const queryHeader = yield csp.take(rChan)
+    while((row = yield csp.take(rChan)) !== 'query close') {
+      queryRows.push(row)
+    }
+
+    yield csp.put(wChan, 'close')
   })
 }
 
-// start server "process"
-mockServer(clientToServerMsgs, serverToClientMsgs)
-// start client
-client(serverToClientMsgs, clientToServerMsgs)
-
-// ... node process should exit when both processes have finished
+const serverDone = mockServer(clientToServerMsgs, serverToClientMsgs)
+const clientDone = client(serverToClientMsgs, clientToServerMsgs)
