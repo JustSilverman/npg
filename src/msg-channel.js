38f76be1a80bdb4ea6e783588bd691db7c45787f
justins <justins@wealthfront.com>
2016-02-12 17:17:49 -0800

    Add msg-channel module

---
+++ src/msg-channel.js
@@ -0,0 +1,121 @@
+import { deepEqual } from 'assert'
+import csp from 'js-csp'
+import * as meta from './meta'
+import readSeq from './msg-sequence'
+import hexBuf from './hex-buf'
+import equalErrors from '../test/helpers/equal-errors'
+
+meta.module(module, {
+  doc: `
+    # Channel that transforms data chunks into messages
+  `,
+})
+
+meta.fn('create', {
+  doc: 'Creates a message channel',
+  shape: 'Channel, int?, int?, bool?  -> Channel',
+  args: [
+    'Channel of binary data',
+    'size of message header, defaults to 1, optional',
+    'size of message length bytes, defaults to 4, optional',
+    'whether the messages length include the length bytes, defaults to true, optional'
+  ],
+  returns: [
+    'Channel of postgres message objects'
+  ],
+  examples: [
+    ['2 complete messages', (f) => {
+      const firstMessage = hexBuf('0a 00 00 00 06 0b 0c')
+      const secondMessage = hexBuf('0a 00 00 00 04')
+      const givenChannel = csp.chan(2)
+      const expectedMessages = [
+        { head: hexBuf('0a'), body: hexBuf('0b 0c') },
+        { head: hexBuf('0a'), body: hexBuf('') }
+      ]
+
+      const messageChannel = f(givenChannel)
+      const postgresMessages = []
+
+      csp.go(function* () {
+        yield csp.put(givenChannel, firstMessage)
+        yield csp.put(givenChannel, secondMessage)
+        postgresMessages.push(yield csp.take(messageChannel))
+        postgresMessages.push(yield csp.take(messageChannel))
+
+        deepEqual(postgresMessages, expectedMessages)
+        console.log('Example passed')
+      })
+    }],
+
+    ['passing an error', (f) => {
+      const firstMessage = hexBuf('0a 00 00 00 06 0b 0c')
+      const givenError = new Error('Error in the channel')
+      const givenChannel = csp.chan(2)
+      const expectedMessage = { head: hexBuf('0a'), body: hexBuf('0b 0c') }
+
+      const messageChannel = f(givenChannel)
+
+      csp.go(function* () {
+        yield csp.put(givenChannel, firstMessage)
+        yield csp.put(givenChannel, givenError)
+        const msg = yield csp.take(messageChannel)
+        deepEqual(expectedMessage, msg)
+
+        const error = yield csp.take(messageChannel)
+        equalErrors(error, givenError)
+
+        console.log('Error example passed')
+      })
+    }],
+
+    ['closed channel', (f) => {
+      const firstMessage = hexBuf('0a 00 00 00 06 0b 0c')
+      const givenChannel = csp.chan(2)
+      const expectedMessage = { head: hexBuf('0a'), body: hexBuf('0b 0c') }
+      const messageChannel = f(givenChannel)
+
+      csp.go(function* () {
+        yield csp.put(givenChannel, firstMessage)
+        givenChannel.close()
+
+        const msg = yield csp.take(messageChannel)
+        deepEqual(expectedMessage, msg)
+
+        const close = yield csp.take(messageChannel)
+        deepEqual(close, null)
+
+        console.log('Closed channel example passed')
+      })
+    }]
+  ],
+})
+
+export const create = (rChan, headLength = 1, lengthBytesCount = 4, lengthBytesInclusive = true) => {
+  const wChan = csp.chan()
+
+  csp.go(function* () {
+    let tempBuf
+    let partialMessage = hexBuf('')
+    while((tempBuf = yield csp.take(rChan)) !== csp.CLOSED) {
+      if (tempBuf instanceof Error) {
+        yield csp.put(wChan, tempBuf)
+        return
+      }
+
+      partialMessage = Buffer.concat([partialMessage, tempBuf])
+      let [ messages, restFnc ] = readSeq(partialMessage, headLength, lengthBytesCount, lengthBytesInclusive)
+
+      for (let msg of messages) {
+        yield csp.put(wChan, msg)
+      }
+
+      partialMessage = restFnc()
+    }
+
+    wChan.close()
+  })
+
+  return wChan
+}
+
+export default create
---
+++ test/src/unit/msg-channel-test.js
@@ -0,0 +1,142 @@
+import { deepEqual } from 'assert'
+import csp from 'js-csp'
+import equalErrors from '../../helpers/equal-errors'
+import hexBuf from '../../../src/hex-buf'
+import create from '../../../src/msg-channel'
+
+describe('msg-channel', () => {
+  describe('#create', () => {
+    it('transforms a channel of buffers into a message channel', (done) => {
+      const firstMessage = hexBuf('0a 00 00 00 06 0b 0c')
+      const secondMessage = hexBuf('0a 00 00 00 04')
+      const givenChannel = csp.chan()
+      const expectedMessages = [
+        { head: hexBuf('0a'), body: hexBuf('0b 0c') },
+        { head: hexBuf('0a'), body: hexBuf('') }
+      ]
+
+      const messageChannel = create(givenChannel)
+      const postgresMessages = []
+
+      csp.go(function* () {
+        yield csp.put(givenChannel, firstMessage)
+        postgresMessages.push(yield csp.take(messageChannel))
+        yield csp.put(givenChannel, secondMessage)
+        postgresMessages.push(yield csp.take(messageChannel))
+
+        deepEqual(postgresMessages, expectedMessages)
+        done()
+      })
+    })
+
+    it('propagates errors', (done) => {
+      const firstMessage = hexBuf('0a 00 00 00 06 0b 0c')
+      const givenError = new Error('Error in the channel')
+      const givenChannel = csp.chan()
+      const expectedMessage = { head: hexBuf('0a'), body: hexBuf('0b 0c') }
+
+      const messageChannel = create(givenChannel)
+
+      csp.go(function* () {
+        yield csp.put(givenChannel, firstMessage)
+        const msg = yield csp.take(messageChannel)
+        deepEqual(expectedMessage, msg)
+        yield csp.put(givenChannel, givenError)
+        const error = yield csp.take(messageChannel)
+
+        equalErrors(error, givenError)
+        done()
+      })
+    })
+
+    it('handles the closing of the read channel', (done) => {
+      const firstMessage = hexBuf('0a 00 00 00 06 0b 0c')
+      const givenChannel = csp.chan()
+      const expectedMessage = { head: hexBuf('0a'), body: hexBuf('0b 0c') }
+      const messageChannel = create(givenChannel)
+
+      csp.go(function* () {
+        yield csp.put(givenChannel, firstMessage)
+        givenChannel.close()
+
+        const msg = yield csp.take(messageChannel)
+        deepEqual(expectedMessage, msg)
+
+        const close = yield csp.take(messageChannel)
+        deepEqual(close, null)
+        done()
+      })
+    })
+
+    context('messages with non-default parameters', () => {
+      it('handles headless messages', (done) => {
+        const firstMessage = hexBuf('00 00 00 06 0b 0c')
+        const secondMessage = hexBuf('00 00 00 04')
+        const givenChannel = csp.chan()
+        const expectedMessages = [
+          { head: null, body: hexBuf('0b 0c') },
+          { head: null, body: hexBuf('') }
+        ]
+
+        const messageChannel = create(givenChannel, 0)
+        const postgresMessages = []
+
+        csp.go(function* () {
+          yield csp.put(givenChannel, firstMessage)
+          postgresMessages.push(yield csp.take(messageChannel))
+          yield csp.put(givenChannel, secondMessage)
+          postgresMessages.push(yield csp.take(messageChannel))
+
+          deepEqual(postgresMessages, expectedMessages)
+          done()
+        })
+      })
+
+      it('handles messages with 2 length bytes', (done) => {
+        const firstMessage = hexBuf('0a 00 03 0b')
+        const secondMessage = hexBuf('0b 00 04 0a 0a')
+        const givenChannel = csp.chan()
+        const expectedMessages = [
+          { head: hexBuf('0a'), body: hexBuf('0b') },
+          { head: hexBuf('0b'), body: hexBuf('0a 0a') }
+        ]
+
+        const messageChannel = create(givenChannel, 1, 2)
+        const postgresMessages = []
+
+        csp.go(function* () {
+          yield csp.put(givenChannel, firstMessage)
+          postgresMessages.push(yield csp.take(messageChannel))
+          yield csp.put(givenChannel, secondMessage)
+          postgresMessages.push(yield csp.take(messageChannel))
+
+          deepEqual(postgresMessages, expectedMessages)
+          done()
+        })
+      })
+
+      it('handles messages with length excluding lengthBytes', (done) => {
+        const firstMessage = hexBuf('0a 00 01 0b')
+        const secondMessage = hexBuf('0b 00 02 0a 0a')
+        const givenChannel = csp.chan()
+        const expectedMessages = [
+          { head: hexBuf('0a'), body: hexBuf('0b') },
+          { head: hexBuf('0b'), body: hexBuf('0a 0a') }
+        ]
+
+        const messageChannel = create(givenChannel, 1, 2, false)
+        const postgresMessages = []
+
+        csp.go(function* () {
+          yield csp.put(givenChannel, firstMessage)
+          postgresMessages.push(yield csp.take(messageChannel))
+          yield csp.put(givenChannel, secondMessage)
+          postgresMessages.push(yield csp.take(messageChannel))
+
+          deepEqual(postgresMessages, expectedMessages)
+          done()
+        })
+      })
+    })
+  })
+})
