83894a04facda8c3170ee7e0efa33dbd97c64e19
justins <justins@wealthfront.com>
2016-02-14 14:18:50 -0800

    Additional message headers

--- src/msg-headers.js
+++ src/msg-headers.js
@@ -7,17 +7,32 @@ export const headerBytesToSym = new Map()
 
 // A entry for each message header we care about
 // pass ascii char as arg to symbol
-export const authenticationOk = Symbol()
+export const authenticationOk = Symbol('R')
 map.set(new Buffer([0x52]), authenticationOk)
 
-export const readyForQuery = Symbol()
-map.set(new Buffer([0x5a]), readyForQuery)
-
-export const sessionConfirm = Symbol()
+export const sessionConfirm = Symbol('N')
 map.set(new Buffer([0x4e]), sessionConfirm)
 
-export const backendKeyData = Symbol()
+export const parameterStatus = Symbol('S')
+map.set(new Buffer([0x53]), parameterStatus)
+
+export const backendKeyData = Symbol('K')
 map.set(new Buffer([0x4b]), backendKeyData)
 
-export const endRows = Symbol()
-map.set(new Buffer([0x43]), endRows)
+export const readyForQuery = Symbol('Z')
+map.set(new Buffer([0x5a]), readyForQuery)
+
+export const query = Symbol('Q')
+map.set(new Buffer([0x51]), query)
+
+export const queryHead = Symbol('T')
+map.set(new Buffer([0x54]), queryHead)
+
+export const queryRow = Symbol('D')
+map.set(new Buffer([0x44]), queryRow)
+
+export const queryClose = Symbol('C')
+map.set(new Buffer([0x43]), queryClose)
+
+export const exit = Symbol('X')
+map.set(new Buffer([0x58]), exit)
