0d2f4eb8eced303bb894ef7f5f56f24d275c968d
justins <justins@wealthfront.com>
2016-02-21 14:00:03 -0800

    Update cps-if-socket to optionally accept channels instead of always creating new ones

--- src/csp-ify-socket.js
+++ src/csp-ify-socket.js
@@ -6,14 +6,20 @@ import hexBuf from './hex-buf'
 meta.module(module, {
   doc: `
     # Process that creates and manages channels to interface with node's socket api
+
+    Default read and write channels are fixed buffer channels
   `,
 })
 
 meta.fn('toChannels', {
   doc: 'Creates channels to wrap the socket api',
-  shape: 'Socket -> { Channel, Channel, Channel, Channel }',
+  shape: 'Socket, Channel?, Channel?, Channel?, Channel? -> { Channel, Channel, Channel, Channel }',
   args: [
-    'Socket to wrap'
+    'Socket to wrap',
+    'Channel for connected event, optional',
+    'Channel for errors, optional',
+    'Channel for reads, optional',
+    'Channel for writes, optional',
   ],
   returns: [
     'Channel to handle connect event',
@@ -28,22 +34,16 @@ meta.fn('toChannels', {
   ],
 })
 
-export const toChannels = (socket) => {
-  const connected = csp.chan();
+export const toChannels = (socket, connected = csp.chan(), errors = csp.chan(), read = csp.chan(100), write = csp.chan(100)) => {
   socket.on('connect', () => csp.putAsync(connected, socket));
-
-  const errors = csp.chan();
-  socket.on('error', (err) => csp.putAsync(errors, err));
-
-  const read = csp.chan();
-  socket.on('data', (buf) => { csp.putAsync(read, buf) });
+  socket.on('error', err => csp.putAsync(errors, err));
+  socket.on('data', buf => csp.putAsync(read, buf));
   socket.on('end', () => { read.close() });
 
-  const write = csp.chan()
   csp.go(function * () {
     let buf
     while ((buf = yield csp.take(write)) && buf !== csp.CLOSED) {
-      socket.write(buf, (err) => { if (err) csp.putAsync(errors, err) })
+      socket.write(buf, err => { if (err) csp.putAsync(errors, err) })
     }
   })
 
--- test/src/unit/cspify-socket-test.js
+++ test/src/unit/cspify-socket-test.js
@@ -7,60 +7,129 @@ import equalErrors from '../../helpers/equal-errors'
 import hexBuf from '../../../src/hex-buf'
 import toChannels from '../../../src/csp-ify-socket'
 
-describe.only('csp-ify-socket', () => {
+describe('csp-ify-socket', () => {
   describe('#toChannels', () => {
-    it('puts the socket itself onto the connected channel on connect', (done) => {
-      const givenSocket = new Duplex()
-      const { connected, errors, read, write } = toChannels(givenSocket)
+    describe('using new channels', () => {
+      it('puts the socket itself onto the connected channel on connect', (done) => {
+        const givenSocket = new Duplex()
+        const { connected, errors, read, write } = toChannels(givenSocket)
 
-      csp.go(function* () {
-        givenSocket.emit('connect')
-        const socket = yield csp.take(connected)
-        deepEqual(socket, givenSocket)
-        done()
+        csp.go(function* () {
+          givenSocket.emit('connect')
+          const socket = yield csp.take(connected)
+          deepEqual(socket, givenSocket)
+          done()
+        })
+      })
+
+      it('puts data onto the read channel from data events', (done) => {
+        const givenSocket = createMockReader([ hexBuf('00'), hexBuf('01 02'), hexBuf('03') ])
+        const expectedAllData = hexBuf('00 01 02 03')
+        const { connected, errors, read, write } = toChannels(givenSocket)
+
+        csp.go(function* () {
+          const allData = []
+          let data
+
+          while((data = yield csp.take(read)) !== csp.CLOSED) {
+            allData.push(data)
+          }
+
+          deepEqual(Buffer.concat(allData), expectedAllData)
+          done()
+        })
+      })
+
+      it('writes data put on the write channel to the socket', (done) => {
+        const givenSocket = createMockWriter()
+        const expectedAllData = hexBuf('00 01 02 03')
+        const { connected, errors, read, write } = toChannels(givenSocket)
+
+        csp.go(function* () {
+          yield csp.operations.onto(write, [ hexBuf('00 01'), hexBuf('02'), hexBuf('03') ])
+          deepEqual(Buffer.concat(givenSocket.testBuffer()), expectedAllData)
+          done()
+        })
+      })
+
+      it('puts errors onto the error channel', (done) => {
+        const givenSocket = new Duplex()
+        const givenError = new Error('Internal error')
+        const { connected, errors, read, write } = toChannels(givenSocket)
+
+        csp.go(function* () {
+          givenSocket.emit('error', givenError)
+          const error = yield csp.take(errors)
+          equalErrors(error, givenError)
+          done()
+        })
       })
     })
 
-    it.only('puts data onto the read channel from data events', (done) => {
-      const givenSocket = createMockReader([ hexBuf('00'), hexBuf('01 02'), hexBuf('03') ])
-      const expectedAllData = hexBuf('00 01 02 03')
-      const { connected, errors, read, write } = toChannels(givenSocket)
+    describe('passing channels as arguments', () => {
+      const _ = undefined
 
-      csp.go(function* () {
-        const allData = []
-        let data
+      it('puts the socket itself onto the connected channel on connect', (done) => {
+        const givenSocket = new Duplex()
+        const existingConnectChannel = csp.chan()
+        const { connected, errors, read, write } = toChannels(givenSocket, existingConnectChannel)
+        deepEqual(connected, existingConnectChannel)
 
-        while((data = yield csp.take(read)) !== csp.CLOSED) {
-          allData.push(data)
-        }
-
-        deepEqual(Buffer.concat(allData), expectedAllData)
-        done()
+        csp.go(function* () {
+          givenSocket.emit('connect')
+          const socket = yield csp.take(existingConnectChannel)
+          deepEqual(socket, givenSocket)
+          done()
+        })
       })
-    })
 
-    it('writes data put on the write channel to the socket', (done) => {
-      const givenSocket = createMockWriter()
-      const expectedAllData = hexBuf('00 01 02 03')
-      const { connected, errors, read, write } = toChannels(givenSocket)
+      it('puts data onto the read channel from data events', (done) => {
+        const givenSocket = createMockReader([ hexBuf('00'), hexBuf('01 02'), hexBuf('03') ])
+        const expectedAllData = hexBuf('00 01 02 03')
+        const existingRead = csp.chan(50)
+        const { connected, errors, read, write } = toChannels(givenSocket, _, _, existingRead)
+        deepEqual(read, existingRead)
 
-      csp.go(function* () {
-        yield csp.operations.onto(write, [ hexBuf('00 01'), hexBuf('02'), hexBuf('03') ])
-        deepEqual(Buffer.concat(givenSocket.testBuffer()), expectedAllData)
-        done()
+        csp.go(function* () {
+          const allData = []
+          let data
+
+          while((data = yield csp.take(existingRead)) !== csp.CLOSED) {
+            allData.push(data)
+          }
+
+          deepEqual(Buffer.concat(allData), expectedAllData)
+          done()
+        })
       })
-    })
 
-    it('puts errors onto the error channel', (done) => {
-      const givenSocket = new Duplex()
-      const givenError = new Error('Internal error')
-      const { connected, errors, read, write } = toChannels(givenSocket)
+      it('writes data put on the write channel to the socket', (done) => {
+        const givenSocket = createMockWriter()
+        const expectedAllData = hexBuf('00 01 02 03')
+        const existingWrite = csp.chan()
+        const { connected, errors, read, write } = toChannels(givenSocket, _, _, _, existingWrite)
+        deepEqual(write, existingWrite)
 
-      csp.go(function* () {
-        givenSocket.emit('error', givenError)
-        const error = yield csp.take(errors)
-        equalErrors(error, givenError)
-        done()
+        csp.go(function* () {
+          yield csp.operations.onto(existingWrite, [ hexBuf('00 01'), hexBuf('02'), hexBuf('03') ])
+          deepEqual(Buffer.concat(givenSocket.testBuffer()), expectedAllData)
+          done()
+        })
+      })
+
+      it('puts errors onto the error channel', (done) => {
+        const givenSocket = new Duplex()
+        const givenError = new Error('Internal error')
+        const existingError = csp.chan()
+        const { connected, errors, read, write } = toChannels(givenSocket, _, existingError)
+        deepEqual(errors, existingError)
+
+        csp.go(function* () {
+          givenSocket.emit('error', givenError)
+          const error = yield csp.take(existingError)
+          equalErrors(error, givenError)
+          done()
+        })
       })
     })
   })
