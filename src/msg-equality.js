6712fdb9cd745bae1d922d79884a2e748c9b4a40
justins <justins@wealthfront.com>
2016-02-21 18:35:43 -0800

    Add docs and tests for msg-equality

--- src/msg-equality.js
+++ src/msg-equality.js
@@ -1,9 +1,61 @@
-import { symToHeaderByte as headers } from './msg-headers'
+import * as assert from 'assert'
+import * as meta from './meta'
+import { read }  from './msg-reader'
+import { hexBuf }  from './hex-buf'
+import * as headers from './msg-headers'
+
+meta.module(module, {
+  doc: `
+    # Postgres message comparators
+  `,
+})
+
+// -
+
+meta.fn('equals', {
+  doc: 'Determines if two messages are identical',
+  shape: 'pg message, pg message -> bool',
+  args: [
+    'pg message',
+    'pg message'
+  ],
+  returns: [
+    'bool'
+  ],
+  examples: {
+    'equal messages': (f) => {
+      const givenMessage1 = { head: hexBuf('0a 00 00 00 05 0b'), body: hexBuf('0b') }
+      const givenMessage2 = { head: hexBuf('0a 00 00 00 05 0b'), body: hexBuf('0b') }
+
+      deepEqual(f(givenMessage1, givenMessage2), true)
+    }
+  },
+})
 
 export const equals = (msg1, msg2) => {
   return msg1.head.equals(msg2.head) && msg1.body.equals(msg2.body)
 }
 
+meta.fn('equals', {
+  doc: 'Determines if a pg message is of a specific pg type',
+  shape: 'symbol, pg message -> bool',
+  args: [
+    'symbol referencing header byte',
+    'pg message'
+  ],
+  returns: [
+    'bool'
+  ],
+  examples: {
+    'ready for query': (f) => {
+      const givenHeadSymbol = headers.readyForQuery
+      const givenMessage = { head: hexBuf('5a'), body: hexBuf('') }
+
+      deepEqual(f(givenHeadSymbol, givenMessage), true)
+    }
+  },
+})
+
 export const ofType = (headSymbol, message) => {
-  return message.head.equals(headers.get(headSymbol))
+  return message.head.equals(headers.symToHeaderByte.get(headSymbol))
 }
---
+++ test/src/unit/msg-equality-test.js
@@ -0,0 +1,56 @@
+import { deepEqual, throws } from 'assert'
+import hexBuf from '../../../src/hex-buf'
+import { equals, ofType } from '../../../src/msg-equality'
+import * as headers from '../../../src/msg-headers'
+
+describe('msg-equality', () => {
+  describe('#equals', () => {
+    it('is true for equal messages', () => {
+      const givenMessage1 = { head: hexBuf('53'), body: hexBuf('1a 2b') }
+      const givenMessage2 = { head: hexBuf('53'), body: hexBuf('1a 2b') }
+      deepEqual(equals(givenMessage1, givenMessage2), true)
+    })
+
+    it('is false when heads do not match', () => {
+      const givenMessage1 = { head: hexBuf('53'), body: hexBuf('1a 2b') }
+      const givenMessage2 = { head: hexBuf('54'), body: hexBuf('1a 2b') }
+      deepEqual(equals(givenMessage1, givenMessage2), false)
+    })
+
+    it('is false when bodies do not match', () => {
+      const givenMessage1 = { head: hexBuf('53'), body: hexBuf('1a 2b') }
+      const givenMessage2 = { head: hexBuf('53'), body: hexBuf('1a') }
+      deepEqual(equals(givenMessage1, givenMessage2), false)
+    })
+  })
+
+  describe('#ofType', () => {
+    it('is true for the correct type', () => {
+      const givenHeadsAndMessages = [
+        [ headers.authenticationOk, { head: hexBuf('52'), body: hexBuf('') } ],
+        [ headers.sessionConfirm, { head: hexBuf('4e'), body: hexBuf('') } ],
+        [ headers.parameterStatus, { head: hexBuf('53'), body: hexBuf('') } ],
+        [ headers.backendKeyData, { head: hexBuf('4b'), body: hexBuf('') } ],
+        [ headers.readyForQuery, { head: hexBuf('5a'), body: hexBuf('') } ],
+        [ headers.query, { head: hexBuf('51'), body: hexBuf('') } ],
+        [ headers.queryHead, { head: hexBuf('54'), body: hexBuf('') } ],
+        [ headers.queryRow, { head: hexBuf('44'), body: hexBuf('') } ],
+        [ headers.queryClose, { head: hexBuf('43'), body: hexBuf('') } ],
+        [ headers.exit, { head: hexBuf('58'), body: hexBuf('') } ]
+      ]
+
+      givenHeadsAndMessages.forEach(([head, msg]) => {
+        deepEqual(ofType(head, msg), true)
+      })
+
+      deepEqual(givenHeadsAndMessages.length, headers.symToHeaderByte.size)
+    })
+
+    it('is false for mismatching types', () => {
+      const givenMessage = { head: hexBuf('53'), body: hexBuf('') }
+      deepEqual(ofType(headers.query, givenMessage), false)
+
+    })
+  })
+
+})
