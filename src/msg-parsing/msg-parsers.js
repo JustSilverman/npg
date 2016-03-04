2a89ca6921dc16fddf5f4082c02e8266b54a9240
justins <justins@wealthfront.com>
2016-03-03 21:39:42 -0800

    Fix imports and invert map to be header symbol -> parser fn.

--- src/msg-parsing/msg-parsers.js
+++ src/msg-parsing/msg-parsers.js
@@ -1,13 +1,13 @@
-import { headers, symToHeaderByte } from '../msg-headers'
-import parse as parseBackendKeyData from './backend-key-data'
-import parse as parseParameterStatus from './parameter-status'
-import parse as parseRowDescription from './row-description'
-import parse as parseDataRow from './data-row'
+import * as headers from '../msg-headers'
+import { parse as parseBackendKeyData } from './backend-key-data'
+import { parse as parseParameterStatus } from './parameter-status'
+import { parse as parseRowDescription } from './row-description'
+import { parse as parseDataRow } from './data-row'
 // A map of header byte to parsing function
 
-export const headerByteToParser = new Map()
+export const headerSymToParser = new Map()
 
-headerByteToParser.put(symToHeaderByte.get(headers.backendKeyData), parseBackendKeyData)
-headerByteToParser.put(symToHeaderByte.get(headers.parameterStatus), parseParameterStatus)
-headerByteToParser.put(symToHeaderByte.get(headers.rowDescription), parseRowDescription)
-headerByteToParser.put(symToHeaderByte.get(headers.dataRow), parseDataRow)
+headerSymToParser.set(headers.backendKeyData, parseBackendKeyData)
+headerSymToParser.set(headers.parameterStatus, parseParameterStatus)
+headerSymToParser.set(headers.rowDescription, parseRowDescription)
+headerSymToParser.set(headers.dataRow, parseDataRow)
