fdff1a21138742244c5bc4262870ae27a953d1c8
justins <justins@wealthfront.com>
2016-03-07 20:39:00 -0800

    Include text description of message in symbol

--- src/msg-headers.js
+++ src/msg-headers.js
@@ -4,42 +4,42 @@
 export const symToHeaderByte = new Map()
 export const headerByteToSym = new Map()
 
-export const authenticationOk = Symbol('R')
+export const authenticationOk = Symbol('authenticationOk (R)')
 symToHeaderByte.set(authenticationOk, new Buffer([0x52]))
 headerByteToSym.set('R', authenticationOk)
 
-export const sessionConfirm = Symbol('N')
+export const sessionConfirm = Symbol('sessionConfirm (N)')
 symToHeaderByte.set(sessionConfirm, new Buffer([0x4e]))
 headerByteToSym.set('N', sessionConfirm)
 
-export const parameterStatus = Symbol('S')
+export const parameterStatus = Symbol('parameterStatus (S)')
 symToHeaderByte.set(parameterStatus, new Buffer([0x53]))
 headerByteToSym.set('S', parameterStatus)
 
-export const backendKeyData = Symbol('K')
+export const backendKeyData = Symbol('backendKeyData (K)')
 symToHeaderByte.set(backendKeyData, new Buffer([0x4b]))
 headerByteToSym.set('K', backendKeyData)
 
-export const readyForQuery = Symbol('Z')
+export const readyForQuery = Symbol('readyForQuery (Z)')
 symToHeaderByte.set(readyForQuery, new Buffer([0x5a]))
 headerByteToSym.set('Z', readyForQuery)
 
-export const query = Symbol('Q')
+export const query = Symbol('query (Q)')
 symToHeaderByte.set(query, new Buffer([0x51]))
 headerByteToSym.set('Q', query)
 
-export const rowDescription = Symbol('T')
+export const rowDescription = Symbol('rowDescription (T)')
 symToHeaderByte.set(rowDescription, new Buffer([0x54]))
 headerByteToSym.set('T', rowDescription)
 
-export const dataRow = Symbol('D')
+export const dataRow = Symbol('dataRow (D)')
 symToHeaderByte.set(dataRow, new Buffer([0x44]))
 headerByteToSym.set('D', dataRow)
 
-export const queryClose = Symbol('C')
+export const queryClose = Symbol('queryClose (C)')
 symToHeaderByte.set(queryClose, new Buffer([0x43]))
 headerByteToSym.set('C', queryClose)
 
-export const exit = Symbol('X')
+export const exit = Symbol('exit (X)')
 symToHeaderByte.set(exit, new Buffer([0x58]))
 headerByteToSym.set('X', exit)
